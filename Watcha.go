b69111d4b1585446dd90ae8ed17ab3d106e95948
HsukqiLee <team@tsinbei.com>
2025-02-04 15:52:12 +0800

    fix: watcha unlock check (v2)

--- Watcha.go
+++ Watcha.go
@@ -29,7 +29,17 @@ if region != "" {
 */
 
 func Watcha(c http.Client) Result {
-	resp, err := GET(c, "https://watcha.com/browse/theater",
+	resp1, err := GET(c, "https://watcha.com/api/aio_browses/tvod/all?size=3")
+	if err != nil {
+		return Result{Status: StatusNetworkErr, Err: err}
+	}
+	defer resp1.Body.Close()
+
+	if resp1.StatusCode == 451 {
+		return Result{Status: StatusNo}
+	}
+
+	resp2, err := GET(c, "https://watcha.com/browse/theater",
 		H{"Connection", "keep-alive"},
 		H{"Upgrade-Insecure-Requests", "1"},
 		H{"Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7"},
@@ -37,22 +47,22 @@ func Watcha(c http.Client) Result {
 	if err != nil {
 		return Result{Status: StatusNetworkErr, Err: err}
 	}
-	defer resp.Body.Close()
+	defer resp2.Body.Close()
 
-	if resp.StatusCode == 451 {
+	if resp2.StatusCode == 451 {
 		return Result{Status: StatusNo}
 	}
-	if resp.StatusCode == 403 {
+	if resp2.StatusCode == 403 {
 		return Result{Status: StatusBanned}
 	}
-	if resp.StatusCode == 200 {
+	if resp2.StatusCode == 200 {
 		return Result{Status: StatusOK, Region: "kr"}
 	}
-	if resp.StatusCode == 302 {
-		if resp.Header.Get("Location") == "https://watcha.com/jp-ja" {
+	if resp2.StatusCode == 302 {
+		if resp2.Header.Get("Location") == "/ja-JP/browse/theater" {
 			return Result{Status: StatusOK, Region: "jp"}
 		}
-		if resp.Header.Get("Location") == "/ko-KR/browse/theater" {
+		if resp2.Header.Get("Location") == "/ko-KR/browse/theater" {
 			return Result{Status: StatusOK, Region: "kr"}
 		}
 	}
