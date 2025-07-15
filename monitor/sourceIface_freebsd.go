755aa2f3a1c01c61c35f8367b6ab36403e2c15d0
HsukqiLee <team@tsinbei.com>
2025-07-15 22:07:20 +0800

    fix security issues

--- monitor/sourceIface_freebsd.go
+++ monitor/sourceIface_freebsd.go
@@ -7,7 +7,6 @@ import (
 	"fmt"
 	"net"
 	"syscall"
-	"unsafe"
 )
 
 func init() {
@@ -37,14 +36,50 @@ func init() {
 				return
 			}
 
-			// 绑定到特定接口
+			// 绑定到特定接口 - 使用更安全的方法
 			if interfaceName != "" {
-				var ifreq [32]byte
-				copy(ifreq[:], interfaceName)
-				_, _, errno := syscall.Syscall(syscall.SYS_IOCTL, fd, syscall.SIOCGIFADDR, uintptr(unsafe.Pointer(&ifreq[0])))
-				if errno != 0 {
-					innerErr = errno
+				// 查找指定接口的信息
+				iface, err := net.InterfaceByName(interfaceName)
+				if err != nil {
+					innerErr = fmt.Errorf("interface %s not found: %v", interfaceName, err)
 					return
+				}
+
+				// 获取接口的IP地址
+				addrs, err := iface.Addrs()
+				if err != nil {
+					innerErr = fmt.Errorf("failed to get interface addresses: %v", err)
+					return
+				}
+
+				// 寻找第一个可用的IP地址进行绑定
+				for _, addr := range addrs {
+					if ipnet, ok := addr.(*net.IPNet); ok {
+						var sockaddr syscall.Sockaddr
+						if ip4 := ipnet.IP.To4(); ip4 != nil {
+							// IPv4地址
+							sa := &syscall.SockaddrInet4{}
+							copy(sa.Addr[:], ip4)
+							sockaddr = sa
+						} else if ip6 := ipnet.IP.To16(); ip6 != nil {
+							// IPv6地址
+							sa := &syscall.SockaddrInet6{}
+							copy(sa.Addr[:], ip6)
+							sockaddr = sa
+						}
+
+						if sockaddr != nil {
+							// 使用标准的bind方法
+							innerErr = syscall.Bind(int(fd), sockaddr)
+							if innerErr == nil {
+								return // 成功绑定
+							}
+						}
+					}
+				}
+
+				if innerErr == nil {
+					innerErr = fmt.Errorf("no suitable address found for interface %s", interfaceName)
 				}
 			}
 		})
